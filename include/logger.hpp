5b15d2962bb16e99fc2d1660f48fca5b179f8306
岡野智之 <84751283+okano-tomoyuki@users.noreply.github.com>
2023-12-08 22:36:12 +0900

    Create logger.hpp

---
+++ include/logger.hpp
@@ -0,0 +1,188 @@
+#ifndef _UTILITY_LOGGER_HPP_
+#define _UTILITY_LOGGER_HPP_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stdexcept>
+#include <memory>
+
+namespace Utility
+{
+
+class Logger final
+{
+
+private:
+    std::stringstream buffer_;
+    std::string separator_;
+    std::string new_line_;
+
+public:
+    enum Color 
+    {
+        WHITE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        RED
+    };
+
+    explicit Logger(const std::string& separator=" ", const std::string& new_line="\n")
+     : separator_(separator), new_line_(new_line)
+    {}
+
+    Logger& flush()
+    {
+        buffer_.str("");
+        buffer_.clear(std::stringstream::goodbit);
+        return *this;
+    }
+
+    template<typename T>
+    Logger& add(const T& val)
+    {
+        buffer_ << val;
+        return *this;
+    }
+
+    template<typename T, typename... Remain>
+    Logger& add(const T& val, const Remain&... remain)
+    {
+        buffer_ << val << separator_;
+        add(remain...);
+        return *this;
+    }
+
+    template<typename T>
+    Logger& add(const std::vector<T>& val)
+    {
+        std::string result;
+        for (const auto& str : val)
+            result += std::to_string(str) + ' ';
+        result.pop_back();
+        buffer_ << "[" << result << "]";
+        return *this;
+    }
+
+    template<typename T, typename... Remain>
+    Logger& add(const std::vector<T>& val, const Remain&... remain)
+    {
+        std::string result;
+        for (const auto& str : val)
+            result += std::to_string(str) + ' ';
+        result.pop_back();
+        buffer_ << "[" << result << "]" << separator_;
+        add(remain...);
+        return *this;
+    }
+
+    Logger& endl()
+    {
+        buffer_ << new_line_;
+        return *this;
+    }
+
+    template<typename T>
+    Logger& add_line(const T& val)
+    {
+        buffer_ << val << new_line_;
+        return *this;
+    }
+
+    template<typename T, typename... Remain>
+    Logger& add_line(const T& val, const Remain&... remain)
+    {
+        buffer_ << val << separator_;
+        add_line(remain...);
+        return *this;
+    }
+
+    template<typename T>
+    Logger& add_line(const std::vector<T>& val)
+    {
+        std::string result;
+        for (const auto& str : val)
+            result += std::to_string(str) + ' ';
+        result.pop_back();
+        buffer_ << "[" << result << "]" << new_line_;
+        return *this;
+    }
+
+    template<typename T, typename... Remain>
+    Logger& add_line(const std::vector<T>& val, const Remain&... remain)
+    {
+        std::string result;
+        for (const auto& str : val)
+            result += std::to_string(str) + ' ';
+        result.pop_back();
+        buffer_ << "[" << result << "]" << new_line_;
+        add_line(remain...);
+        return *this;
+    }
+
+    Logger& set_separator(const std::string& separator)
+    {
+        separator_ = separator;
+        return *this;
+    }
+
+    Logger& set_new_line(const std::string& new_line)
+    {
+        new_line_ = new_line;
+        return *this;
+    }
+
+    Logger& print(const enum Color& color=WHITE)
+    {
+        std::string escape_color;
+        if(color==WHITE)
+            escape_color = "\033[m";
+        else if(color==YELLOW)
+            escape_color = "\033[33m";
+        else if(color==GREEN)
+            escape_color = "\033[32m";
+        else if(color==BLUE)
+            escape_color = "\033[34m";
+        else if(color==RED)
+            escape_color = "\033[31m";
+        std::cout << escape_color << buffer_.str() << "\033[m";
+        return *this;
+    }
+
+    Logger& save(const std::string& file_path, const std::ios_base::openmode& mode = std::ios_base::app)
+    {
+        std::ofstream ofs;
+        ofs.open(file_path, mode);
+        ofs << buffer_.str();
+        return *this;
+    }
+
+    size_t size() const
+    {
+        return buffer_.str().size();
+    }
+
+    template<typename... Args>
+    static std::string format(const std::string& format, const Args&... args )
+    {
+        int size_s = std::snprintf(nullptr, 0, format.c_str(), args... ) + 1;
+        if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
+        auto size = static_cast<size_t>(size_s);
+        std::unique_ptr<char[]> buf( new char[ size ] );
+        std::snprintf(buf.get(), size, format.c_str(), args...);
+        return std::string(buf.get(), buf.get() + size - 1 );
+    }
+
+    std::string get_buffer() const
+    {
+        return buffer_.str();
+    }
+
+};
+
+}
+
+#endif
